9ab88631c243ba828488117746c782419a945a4d
emeneghim <47820490+emeneghim@users.noreply.github.com>
2019-05-09 11:10:50 -0300

    Add files via upload

--- Exercícios dia 20.03/teste.py
+++ Exercícios dia 20.03/teste.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect
 app = Flask(__name__, static_url_path='/static')
+app.config["SECRET_KEY"] = "Meneghim"
 
 lista = [("Joao","Rua 9", "3322-5152"),
          ("Maria","Rua 11", "99982-7080"),
          ("Eugenia", "Rua 13", "3028-5612")]
-@app.route("/", methods=['POST','GET'])
+@app.route("/")
 def inicio():
     return render_template("inicio.html", lista = lista)
 
@@ -16,5 +17,55 @@ def easter():
 def exibir():
     return render_template('exibir_mensagem.html', lista = lista)
 
+@app.route('/listar_pessoas/')
+def listar_pessoas():
+    return render_template('listar_pessoas.html', lista = lista)
+
+@app.route('/incluir_pessoa', methods = ["POST"])
+def incluir_pessoa():
+    nome = request.form["nome"]
+    ender = request.form["endereco"]
+    tel = request.form["telefone"]
+    print ("Teste 2")
+    nova = (nome, ender, tel)
+    lista.append(nova)
+    return render_template('exibir_mensagem.html', lista = lista)
+
+@app.route("/excluir_pessoa")
+def excluir():
+    achou = None
+    nome = request.args.get("nome")
+    for p in lista:
+        if p[0] == nome:
+            achou = p
+            break
+    if achou != None and session["usuario"]: 
+        lista.remove(achou)
+    return render_template('exibir_mensagem.html', lista = lista)    
+
+@app.route("/form_alterar_pessoa")
+def form_alterar_pessoa():
+    procurado = request.args.get("nome")
+    for pe in lista:
+        if pe[0] == procurado:
+            pessoa = pe
+            print(pessoa)
+    return "não achei: " +str(procurado)
+
+@app.route("/login", methods = ["POST"])
+def login():
+    login = request.form["login"]
+    senha = request.form["senha"]
+    if login == "dudu" and senha == "123":
+        session["usuario"] = login
+        return redirect("/")
+    else:
+        return ("login/senha inválidos!")
+
+@app.route("/logout")
+def logout():
+    session.pop("usuario")
+    return redirect("/")
+
 if __name__ == "__main__":
-    app.run(use_reloader = True)
+    app.run(use_reloader = True, debug = True)  
